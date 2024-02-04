07e844f3448a7e2c6b90fc19d4ac40ee2db8bf9b
Vermaas <vermaas@astron.nl>
2024-02-04 18:58:42 +0100

    switch to MCV pattern

--- src/routes.php
+++ src/routes.php
@@ -6,3 +6,5 @@ use MVC\Controllers\HikeController;
 $router = new Router();
 
 $router->addRoute('/', HikeController::class, 'index');
+$router->addRoute('/my_hiking/', HikeController::class, 'index');
+return $router;
