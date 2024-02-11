2fae315a4a18f5c88a7468c59b67509baa9eb68e
Vermaas <vermaas@astron.nl>
2024-02-11 11:39:00 +0100

    add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vermaas/my_hiking',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '820d40c60d35f58208cd2bc031b17eeaa93324c9',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vermaas/my_hiking' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '820d40c60d35f58208cd2bc031b17eeaa93324c9',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
